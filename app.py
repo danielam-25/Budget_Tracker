1cb7dd1f95c70821ff19d05bff8ef3f4e6e2029d
danielam-25 <marquez.daniela005@gmail.com>
2025-12-18 00:23:00 -0800

    yes

--- app.py
+++ app.py
@@ -60,6 +60,4 @@ def create_budget():
     return redirect(url_for('add_budget_page'))
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
